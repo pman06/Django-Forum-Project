e0a19987c1e0a172a2efd804fb0f21a68b22d1ff
paulino007 <paul@paulshoremekun.com>
2020-09-17 18:13:37 -0600

    Implemented the signup form and bootstrap, and added more tests to ascertain all part of the signup forms are wroking perfectly...implemented logout views, to be implemented in next commit

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,12 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 from django.urls import path
 from board import views
+from accounts import views as accounts_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name = 'home'),
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
     path('boards/<int:pk>/new/', views.new_topic, name ='new_topic'),
+    path('logout/',auth_views.LogoutView.as_view(), name = 'logout'),
+    path('signup/', accounts_views.signup, name='signup'),
+    path('admin/', admin.site.urls),
 ]
